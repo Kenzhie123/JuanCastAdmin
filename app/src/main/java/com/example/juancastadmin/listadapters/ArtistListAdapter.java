ffb8719b5012890dae50cc7e555795f61e5b5f29
Kenzhie123 <73787050+Kenzhie123@users.noreply.github.com>
2024-07-25 05:26:10 +0800

    Removal of artistProfile
    Removed artistProfile variable and adding of artistID variable

--- app/src/main/java/com/example/juancastadmin/listadapters/ArtistListAdapter.java
+++ app/src/main/java/com/example/juancastadmin/listadapters/ArtistListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.juancastadmin.listadapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,16 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.example.juancastadmin.AddArtist;
 import com.example.juancastadmin.R;
 import com.example.juancastadmin.model.Artist;
 import com.google.firebase.storage.FirebaseStorage;
@@ -48,7 +53,7 @@ public class ArtistListAdapter extends RecyclerView.Adapter<ArtistListAdapter.Ar
     public void onBindViewHolder(@NonNull ArtistListViewHolder holder, int position) {
 
         Artist currentArtist = artistList.get(holder.getAdapterPosition());
-        StorageReference reference = storage.getReference().child("artists/" + currentArtist.getArtistProfile());
+        StorageReference reference = storage.getReference().child("artists/" + currentArtist.getArtistID());
         String tagsText = "";
 
         for(String tag : currentArtist.getTags())
@@ -59,7 +64,8 @@ public class ArtistListAdapter extends RecyclerView.Adapter<ArtistListAdapter.Ar
         holder.ARI_Name.setText(currentArtist.getArtistName());
         holder.ARI_Tags.setText(tagsText);
 
-        Glide.with(context).load(reference).addListener(new RequestListener<Drawable>() {
+        Glide.with(context).load(reference).diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true).addListener(new RequestListener<Drawable>() {
             @Override
             public boolean onLoadFailed(@Nullable GlideException e, @Nullable Object model, @NonNull Target<Drawable> target, boolean isFirstResource) {
                 Log.d("DataTag",e.getMessage());
@@ -72,6 +78,19 @@ public class ArtistListAdapter extends RecyclerView.Adapter<ArtistListAdapter.Ar
                 return false;
             }
         }).into(holder.ARI_ProfileImage);
+
+        holder.ARI_Background.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent update = new Intent(context, AddArtist.class);
+                update.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                update.putExtra("isUpdate",true);
+                update.putExtra("artistID",currentArtist.getArtistID());
+                Log.d("DataTag",currentArtist.getArtistID());
+                context.startActivity(update);
+            }
+        });
     }
 
     @Override
@@ -81,7 +100,7 @@ public class ArtistListAdapter extends RecyclerView.Adapter<ArtistListAdapter.Ar
     }
 
     public class ArtistListViewHolder extends RecyclerView.ViewHolder {
-
+        ConstraintLayout ARI_Background;
         TextView ARI_Name;
         ImageView ARI_ProfileImage;
         TextView ARI_Tags;
@@ -91,6 +110,7 @@ public class ArtistListAdapter extends RecyclerView.Adapter<ArtistListAdapter.Ar
             ARI_Name = itemView.findViewById(R.id.ARI_Name);
             ARI_ProfileImage = itemView.findViewById(R.id.ARI_ProfileImage);
             ARI_Tags = itemView.findViewById(R.id.ARI_Tags);
+            ARI_Background = itemView.findViewById(R.id.ARI_Background);
         }
     }
 }
